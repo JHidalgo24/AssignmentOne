ff6e743c7be8b9d4f594f67aa4f422fed7428da4
JHidalgo24 <jhidalgo2@my.wctc.edu>
2021-09-02 01:02:32 -0500

    finished project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+
 
 namespace AssignmentOne
 {
@@ -7,9 +9,8 @@ namespace AssignmentOne
     {
         static void Main(string[] args)
         {
-           String doAgain = "Y";
 
-            while(doAgain.Substring(0,1) == "Y"){
+
             //ask user if they want to read file or write to file
             Console.WriteLine("1)Do you want to read file?");
             System.Console.WriteLine("2)Do you want to write to file?");
@@ -18,43 +19,79 @@ namespace AssignmentOne
             //create variables for the file that is going to be created or already created
             string file = "Tickets.csv";
             //they want to read from file
-                       StreamWriter sw = new StreamWriter(file);
-                       StreamReader sr = new StreamReader(file);
-            if (decision == "1")
-            {
-                if (File.Exists(file))
-                {
-                   //read data from the csv
-                   int count = 0;
-                   while(!sr.EndOfStream){
-                       string line = sr.ReadLine();
-                       //convert string to an array
-                       string[] words = line.Split(',');
-                       //display array data
-                       for(int i = 0; i < words.Length; i++){
-                           System.Console.Write(words[i] + " ");
-                           count++;
-                           if(count == 7){
-                            System.Console.WriteLine();
-                            count = 0;
-                           }
-                       }
-                       sr.Close();
-                   } 
-                }
-                else
-                {
-                    Console.WriteLine("File does not exist!");
-                }
+            int count = 0;
 
-            }
-            else if (decision == "2"){
-                
-                //create accumulator for ticket id
-                int ticketID = 0;
-                do{
-                    
-                    sw.WriteLine("TicketID,Summary,Status,Priority,Submitter,Assigned,Watching");
+            while (decision == "1" || decision == "2")
+            {
+                if (decision == "1")
+                {
+                    if (File.Exists(file))
+                    {
+                        StreamReader sr = new StreamReader(file);
+                        //read data from the csv
+                        sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            //convert string to an array
+                            string[] words = line.Split(',');
+                            //display array data
+                            for (int i = 0; i < words.Length; i++)
+                            {
+                                System.Console.Write(words[i] + ", ");
+                                count++;
+                                if (count == 6)
+                                {
+                                    System.Console.WriteLine();
+                                    count = 0;
+                                }
+                            }
+                        }
+                        sr.Close();
+                    }
+                    else
+                    {
+                        Console.WriteLine("File does not exist!");
+                    }
+
+                }
+                else if (decision == "2")
+                {
+                    if (!File.Exists(file))
+                    {
+                        StreamWriter sf = new StreamWriter(file);
+                        sf.WriteLine("TicketID,Summary,Status,Priority,Submitter,Assigned,Watching");
+                        sf.Close();
+                    }
+                    //create accumulator for ticket id
+                    StreamReader sr = new StreamReader(file);
+                    sr.ReadLine();
+                    int val = 0;
+                    int ticketID = 0;
+                    while (!sr.EndOfStream)
+                    {
+
+                        string line = sr.ReadLine();
+                        //convert string to an array
+                        string[] words = line.Split(',');
+                        //display array data
+                        val = Int32.Parse(words[0]);
+                        for (int i = 0; i < words.Length; i++)
+                        {
+                            if (ticketID <= val)
+                            {
+                                ticketID = val + 1;
+                            }
+
+                        }
+
+                    }
+
+                    sr.Close();
+
+
+                    val++;
+                    StreamWriter sw = new StreamWriter(file, true);
                     //ask for summarry 
                     Console.WriteLine("What is the summary?");
                     //save summary string
@@ -76,28 +113,34 @@ namespace AssignmentOne
                     //save assigned string
                     string assigned = Console.ReadLine();
                     //ask for watching names
-                    System.Console.WriteLine("How many are watching?");        
+                    System.Console.WriteLine("How many are watching?");
                     int watching = Int32.Parse(Console.ReadLine());
                     string watchers = "";
                     //make loop to continue asking for names
-                    for(int i = 0;i < watching; i++){
-                        System.Console.WriteLine($"What is person's {i+1} name?");
+                    for (int i = 0; i < watching; i++)
+                    {
+                        System.Console.WriteLine($"What is person's {i + 1} name?");
                         watchers += Console.ReadLine() + "|";
                     }
                     //write all info to the line
                     sw.WriteLine($"{ticketID},{summary},{status},{priority},{submitter},{watchers}");
                     ticketID++;
+                    watchers = "";
+                    sw.Close();
 
-                }while(decision != "2");
-                sw.Close();
+                }
+                else
+                {
+                    System.Console.WriteLine("That isn't an option sorry input again");
+                    decision = Console.ReadLine();
+                }
+
+                Console.WriteLine("1)Do you want to read file?");
+                System.Console.WriteLine("2)Do you want to write to file?");
+                System.Console.WriteLine("Enter something else to end!");
+                //save the input of the user
+                decision = Console.ReadLine();
             }
-            else{
-            System.Console.WriteLine("That isn't an option sorry input again");
-            decision = Console.ReadLine();}
-
-                System.Console.WriteLine("Do you want to go again?(Y/N)");
-                doAgain = Console.ReadLine();
-        }
         }
     }
 }
